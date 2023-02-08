9de65496936ab0cea5d91a2af9ad41a9ca8111f2
Andrea <60261353+andre91a@users.noreply.github.com>
2023-02-08 15:56:33 +0100

    Adding queryName parameter

--- backend/routeHandler.js
+++ backend/routeHandler.js
@@ -2,7 +2,7 @@
 const bodyParser = require('body-parser');
 
 module.exports = function configureRoutes(options) {
-  options.router.use(bodyParser.json({limit: '50mb', extended: true}));
+  options.router.use(bodyParser.json({ limit: '50mb', extended: true }));
 
   /**
    * sanitize the templateData attribute for runQueryByID parameter
@@ -31,18 +31,18 @@ module.exports = function configureRoutes(options) {
    */
   function checkPluginsConfiguration(schemaTypes, entityType, itemType, properties, delimiter) {
     if (entityType && entityType !== 'node' && entityType !== 'edge') {
-      return {message: 'Invalid plugin configuration “entityType” (must be “node” or “edge”)'};
+      return { message: 'Invalid plugin configuration “entityType” (must be “node” or “edge”)' };
     }
     if (itemType === undefined) {
-      return {message: 'Missing plugin configuration “itemType”'};
+      return { message: 'Missing plugin configuration “itemType”' };
     } else if (!schemaTypes.some((type => type.itemType === itemType))) {
-      return {message: 'Invalid plugin configuration “itemType” (must be an existing node category or edge type)'};
+      return { message: 'Invalid plugin configuration “itemType” (must be an existing node category or edge type)' };
     }
     if (properties && (!Array.isArray(properties) || properties.length === 0)) {
-      return {message: 'Invalid plugin configuration “properties” (must be a non-empty array of property names)'};
+      return { message: 'Invalid plugin configuration “properties” (must be a non-empty array of property names)' };
     }
     if (delimiter && delimiter.length != 1) {
-      return {message: 'Invalid plugin configuration “delimiter” (only one character is allowed)'};
+      return { message: 'Invalid plugin configuration “delimiter” (only one character is allowed)' };
     }
     return null;
   }
@@ -56,7 +56,7 @@ module.exports = function configureRoutes(options) {
     const error = checkPluginsConfiguration(schemaTypes, entityType, itemType, properties, delimiter);
     if (error) {
       res.status(412);
-      res.send(JSON.stringify({status: 412, body: {error}}));
+      res.send(JSON.stringify({ status: 412, body: { error } }));
     } else {
       res.status(200);
       res.send(JSON.stringify(options.configuration));
@@ -73,6 +73,50 @@ module.exports = function configureRoutes(options) {
     res.contentType('application/json');
     res.send(JSON.stringify(query));
   });
+
+  options.router.post('/getQueryByName', async (req, res) => {
+    try {
+    // Checking templates
+      const getQueryParams = {
+        sourceKey: req.body.sourceKey,
+        type: "template"
+      };
+
+      let response = (await options.getRestClient(req).graphQuery.getQueries(getQueryParams));
+
+      let queries = response.body.filter((q) => {
+        return q.name.toLowerCase() === req.body.name;
+      });
+
+      // Checking statics
+      getQueryParams.type = "static"
+
+      response = (await options.getRestClient(req).graphQuery.getQueries(getQueryParams));
+
+      queries = queries.concat(response.body.filter((q) => {
+        return q.name === req.body.name;
+      }));
+
+      if (queries.length === 0) {
+        throw new Error(`Query ${req.body.name} either does not exist or you do not have access to it.`);
+      }
+      else if (queries.length === 1) {
+        response.body = queries[0]
+        res.status(200);
+        res.contentType('application/json');
+        res.send(JSON.stringify(response));
+      }
+      else {
+        throw new Error(`Multiple queries named ${req.body.name} exists. The query name must be unique.`);
+      }
+    } catch (error) {
+      res.status(400);
+      res.contentType('application/json');
+      res.send(JSON.stringify({ status: 400, body: { error: {message: error.message } }));
+    }
+
+  });
+
 
   options.router.post('/runQueryByIDPlugin', async (req, res) => {
     const data = {
@@ -92,7 +136,7 @@ module.exports = function configureRoutes(options) {
       res.status(400);
       res.contentType('application/json');
       const error = e.originalResponse.body ? e.originalResponse.body : e;
-      res.send(JSON.stringify({status: 400, body: {error}}));
+      res.send(JSON.stringify({ status: 400, body: { error } }));
     }
 
   });
@@ -108,7 +152,7 @@ module.exports = function configureRoutes(options) {
       res.send(JSON.stringify(schemaResult));
     } catch (e) {
       res.status(412);
-      res.send(JSON.stringify({status: 412, body: e}));
+      res.send(JSON.stringify({ status: 412, body: e }));
     }
   });
 };
